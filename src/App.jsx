23c345db6e3a6cee08006a911dce67966394593c
AshKemp <kmuraliashwin@gmail.com>
2025-02-09 07:14:49 +0530

    trending movies feature

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Search from "./components/Search";
 import Spinner from "./components/Spinner";
 import MovieCard from "./components/MovieCard";
 import { useDebounce } from "react-use";
+import { updateSearchCount } from "./appWrite";
 
 const API_BASE_URL = "https://api.themoviedb.org/3";
 
@@ -45,6 +46,9 @@ function App() {
         return;
       }
       setMovieList(data.results || []);
+      if (query && data.results.length > 0) {
+        await updateSearchCount(query, data.results[0]);
+      }
     } catch (error) {
       console.error(`Error fetching movies: ${error}`);
       setErrorMessage("An error occurred fetching movies. Please try again.");
